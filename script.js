6e746b827b294f539c71131f442e689da8800faf
linkuello <144430394+linkuello@users.noreply.github.com>
2024-11-25 22:55:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
-    alert('Добро пожаловать в презентацию DynamoDB!');
+    const links = document.querySelectorAll('.sidebar a');
+    const slides = document.querySelectorAll('.slide');
+
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            
+            // Удалить класс 'active' у всех слайдов
+            slides.forEach(slide => slide.classList.remove('active'));
+
+            // Получить номер слайда из data-slide
+            const slideNumber = link.getAttribute('data-slide');
+            const targetSlide = document.querySelector(`#slide-${slideNumber}`);
+
+            // Добавить класс 'active' к выбранному слайду
+            targetSlide.classList.add('active');
+        });
+    });
 });
